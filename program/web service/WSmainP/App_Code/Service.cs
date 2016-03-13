588da437a6e5b39548f9cdd46bffd4936ba42180
Paulo Henrique Piaunios <paaullo.h@gmail.com>
2016-03-13 14:36:08 -0300

    Web Service
    Código do Web Service ASP.NET feito no Visual Studio 2013, gerando xml
    de perfil do usuário.

---
+++ program/web service/WSmainP/App_Code/Service.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data.MySqlClient;
+using System.Xml.Serialization;
+
+[WebService(Namespace = "http://nsmainp.com/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    private MySqlConnection Conn;
+
+    [WebMethod]
+    public User CarregarPerfil()
+    {
+        User user = new User();
+
+        MySqlConnection Conn = new MySqlConnection("Server=localhost;Database=NSmainP;UID=root;Password=1234");
+
+        //Sql Command
+        MySqlCommand cmd = new MySqlCommand("SELECT * FROM USER", Conn);
+
+        Conn.Open();
+
+        //To Read From SQL Server
+        MySqlDataReader dr = cmd.ExecuteReader();
+
+        if (dr.Read())
+        {
+            user.ID = dr["ID"].ToString();
+            user.LOGIN = dr["LOGIN"].ToString();
+            user.SENHA = dr["SENHA"].ToString();
+            user.NOME = dr["NOME"].ToString();
+            user.SEXO = dr["SEXO"].ToString();
+            user.FACE = dr["FACE"].ToString();
+            user.FACEpublic = dr["FACEpublic"].ToString();
+            user.WPP = dr["WPP"].ToString();
+            user.WPPpublic = dr["WPPpublic"].ToString();
+            user.INSTA = dr["INSTA"].ToString();
+            user.INSTApublic = dr["INSTApublic"].ToString();
+            user.SNAP = dr["SNAP"].ToString();
+            user.SNAPpublic = dr["SNAPpublic"].ToString();
+            user.TWITTER = dr["TWITTER"].ToString();
+            user.TWITTERpublic = dr["TWITTERpublic"].ToString();
+            user.EMAIL = dr["EMAIL"].ToString();
+            user.EMAILpublic = dr["EMAILpublic"].ToString();
+            user.LINK = dr["LINK"].ToString();
+            user.LINKpublic = dr["LINKpublic"].ToString();
+            user.DTINSCRICAO = dr["DTINSCRICAO"].ToString();
+        }
+
+        dr.Close();
+        Conn.Close();
+        return user;
+    }
+
+    public class User
+    {
+               
+        public string ID { get; set; }
+        public string LOGIN { get; set; }
+        public string SENHA { get; set; }
+        public string NOME { get; set; }
+        public string SEXO { get; set; }
+        public string FACE { get; set; }
+        public string FACEpublic { get; set; }
+        public string WPP  { get; set; }
+        public string WPPpublic { get; set; }
+        public string INSTA { get; set; }
+        public string INSTApublic { get; set; }
+        public string SNAP { get; set; }
+        public string SNAPpublic { get; set; }
+        public string TWITTER { get; set; }
+        public string TWITTERpublic { get; set; }
+        public string EMAIL { get; set; }
+        public string EMAILpublic { get; set; }
+        public string LINK { get; set; }
+        public string LINKpublic { get; set; }
+        public string DTINSCRICAO { get; set; }
+
+    }
+    
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";
+    }    
+}
