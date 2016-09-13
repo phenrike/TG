face50f9a7c41ef3dd8979411c059e24473862ea
Paulo Henrique Piaunios <paaullo.h@gmail.com>
2016-09-13 19:39:04 -0300

    Criação da classe User
    Foi criada uma classe User separada da classe principal.

--- program/web service/WSmainP/App_Code/Service.cs
+++ program/web service/WSmainP/App_Code/Service.cs
@@ -24,8 +24,6 @@ public class Service : System.Web.Services.WebService
         
     }
 
-    private MySqlConnection Conn;
-
     [WebMethod]
     [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
     public string teste()
@@ -124,32 +122,6 @@ public class Service : System.Web.Services.WebService
         Conn.Close();
 
         return user;
-    }
-
-    public class User
-    {
-               
-        public string ID { get; set; }
-        public string LOGIN { get; set; }
-        public string SENHA { get; set; }
-        public string NOME { get; set; }
-        public string SEXO { get; set; }
-        public string FACE { get; set; }
-        public string FACEpublic { get; set; }
-        public string WPP  { get; set; }
-        public string WPPpublic { get; set; }
-        public string INSTA { get; set; }
-        public string INSTApublic { get; set; }
-        public string SNAP { get; set; }
-        public string SNAPpublic { get; set; }
-        public string TWITTER { get; set; }
-        public string TWITTERpublic { get; set; }
-        public string EMAIL { get; set; }
-        public string EMAILpublic { get; set; }
-        public string LINK { get; set; }
-        public string LINKpublic { get; set; }
-        public string DTINSCRICAO { get; set; }
-
     }
 
     [WebMethod]
---
+++ program/web service/WSmainP/App_Code/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for User
+/// </summary>
+public class User
+{
+
+    public string ID { get; set; }
+    public string LOGIN { get; set; }
+    public string SENHA { get; set; }
+    public string NOME { get; set; }
+    public string SEXO { get; set; }
+    public string FACE { get; set; }
+    public string FACEpublic { get; set; }
+    public string WPP { get; set; }
+    public string WPPpublic { get; set; }
+    public string INSTA { get; set; }
+    public string INSTApublic { get; set; }
+    public string SNAP { get; set; }
+    public string SNAPpublic { get; set; }
+    public string TWITTER { get; set; }
+    public string TWITTERpublic { get; set; }
+    public string EMAIL { get; set; }
+    public string EMAILpublic { get; set; }
+    public string LINK { get; set; }
+    public string LINKpublic { get; set; }
+    public string DTINSCRICAO { get; set; }
+
+}
