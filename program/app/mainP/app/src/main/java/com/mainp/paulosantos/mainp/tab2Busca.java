a551087cab664e4cf809e98d8054efa4a8d6223e
Paulo Henrique Piaunios <paaullo.h@gmail.com>
2017-02-28 13:00:10 -0300

    Desenvolvimento do app.
    Criação do "combobox" das redes sociais para a busca. Troca do logotipo
    do app. Alteração das posições dos componentes da tela de perfil para
    deixa-los mais alinhados.

--- program/app/mainP/app/src/main/java/com/mainp/paulosantos/mainp/tab2Busca.java
+++ program/app/mainP/app/src/main/java/com/mainp/paulosantos/mainp/tab2Busca.java
@@ -9,14 +9,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class tab2Busca extends Fragment {
+
+    Spinner redes;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab2busca, container, false);
+
+        redes = (Spinner) rootView.findViewById(R.id.spRedes);
+        ImageButton btbuscar = (ImageButton) rootView.findViewById(R.id.btBuscar);
+        ArrayAdapter aa = ArrayAdapter.createFromResource(getActivity().getApplicationContext(), R.array.redes_sociais,android.R.layout.simple_spinner_item);
+        redes.setAdapter(aa);
+        //btbuscar.setOnClickListener((View.OnClickListener) getView());
         return rootView;
     }
 
+    /*public void onClick(View v) {
+        int item = redes.getSelectedItemPosition();
+        Toast.makeText(getActivity().getApplicationContext(), "Item escolhido" + Integer.toString(item), Toast.LENGTH_SHORT).show();
+    }*/
 }
